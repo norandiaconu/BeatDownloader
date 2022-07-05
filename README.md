4006284810f24af472ce8159cf1d5890f6767a29
Noran Diaconu <norandiaconu@gmail.com>
2022-07-05 19:32:27 -0400

    Allow for saving directory

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BeatDownloader 1.1.0
+# BeatDownloader 1.1.3
 
 This utility allows you to download all of the ranked custom levels that you don't currently have for Beat Saber.
 
@@ -6,15 +6,8 @@ This utility allows you to download all of the ranked custom levels that you don
 
 `npm i beat-downloader`
 
-## Usage
+---
 
-- ### Want to use the default Beat Saber install directory (C:/Program Files (x86)/Steam/steamapps/common/Beat Saber/Beat Saber_Data/CustomLevels)?
-  - `npx beat-downloader`
-- ### Prefer to provide your own? (Please include /Beat Saber_Data/CustomLevels)
-  - `npx beat-downloader C:\myFavouriteDirectory`
-
-## Everything below is now deprecated and no longer needed
-<img src="https://i.imgur.com/fz0kwft.png" alt="BeatDownloader"/>
 ## Prerequisites
 ### ModAssistant
 If you haven't used this before, make sure to run `ModAssistant.exe` and click the "Install or Update" button in the bottom right.
@@ -23,7 +16,33 @@ On the Options page, make sure the "BeatSaver" option is checked on the "Enable 
 
 Also on the Options page, change the "Show OneClick Installer Window" from the default "Yes" to "Close" or "No". Otherwise you will be left at the end with many open windows of the OneClick Installer.
 
-Current release: https://github.com/Assistant/ModAssistant/releases/download/v1.1.16/ModAssistant.exe  
+Current release: https://github.com/Assistant/ModAssistant/releases/download/v1.1.27/ModAssistant.exe
+
+---
+
+## Usage
+
+- ### Want to use the default Beat Saber install directory (C:/Program Files (x86)/Steam/steamapps/common/Beat Saber/Beat Saber_Data/CustomLevels)?
+  - `npx beat-downloader`
+- ### Prefer to provide your own? (Please include /Beat Saber_Data/CustomLevels)
+  - `npx beat-downloader "C:/Beat Saber/Beat Saber_Data/CustomLevels"`
+  - After providing the directory, you can call just `npx beat-downloader`. This will be saved until you pass a new directory or if there was an error finding the directory.
+
+<img src="https://i.imgur.com/4pnp8Ny.png" alt="beat-downloader"/>
+
+---
+
+## Credits
+Beat Saver: https://beatsaver.com/  
+Beat Savior: https://beat-savior.herokuapp.com/  
+ModAssistant: https://github.com/Assistant/ModAssistant  
+
+## Everything below is now deprecated and no longer needed
+
+---
+
+<img src="https://i.imgur.com/fz0kwft.png" alt="BeatDownloader"/>
+  
 ### BeatDownloader.bat
 This can be used as-is if you have used ModAssistant before. BeatDownloader will use the same install directory as set in ModAssistant.
 ### Xidel
@@ -35,7 +54,4 @@ With all that out of the way, just double-click `BeatDownloader.bat` and the com
 
 Note: As of writing there are currently 689 ranked custom levels, so this will take a while to complete. Closing the command prompt window will stop the process from running, but this can be restarted at a later time skipping the custom levels that have already been downloaded.
 ## Credits
-Beat Saver: https://beatsaver.com/  
-Beat Savior: https://beat-savior.herokuapp.com/  
-ModAssistant: https://github.com/Assistant/ModAssistant  
 Xidel: https://github.com/benibela/xidel
--- index.mjs
+++ index.mjs
@@ -1,100 +1,109 @@
 #!/usr/bin/env node
 import fetch from "node-fetch";
-import { createRequire } from 'module';
+import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const shellExec = require('child_process');
+const shellExec = require("child_process");
 const fs = require("fs");
 const storage = require("node-persist");
 const readline = require("readline");
 
 main();
 async function main() {
-  var directory = process.argv.slice(2).join(" ");
-  if (!directory) {
-    directory = "C:/Program Files (x86)/Steam/steamapps/common/Beat Saber/Beat Saber_Data/CustomLevels";
-  }
-  if (!fs.existsSync(directory)) {
-    console.log("Unavailable directory selected, exiting")
-    return;
-  }
-  console.log("Using the following directory to save maps:\n", directory);
-  await storage.init();
-  let page = await storage.getItem("page");
-  if (page) {
-    const answer = await restart("Do you want to continue previous run? (Enter y/n followed by ENTER key)");
-    if (answer !== "y") {
-      page = 1;
-      await storage.removeItem("page");
+    var directory = process.argv.slice(2).join(" ");
+    await storage.init();
+    if (!directory) {
+        const loadDir = await storage.getItem("directory");
+        if (loadDir) {
+            directory = loadDir;
+        } else {
+            directory = "C:/Program Files (x86)/Steam/steamapps/common/Beat Saber/Beat Saber_Data/CustomLevels";
+        }
+    }
+    if (!fs.existsSync(directory)) {
+        console.log("Unavailable directory selected, exiting");
+        return;
+    }
+    console.log("Using the following directory to save maps:\n", directory);
+    await storage.setItem("directory", directory);
+
+    let page = await storage.getItem("page");
+    if (page) {
+        const answer = await restart("Do you want to continue the previous run on page " + page + "? (Enter y/n followed by ENTER key)");
+        if (answer !== "y") {
+            page = 1;
+            await storage.removeItem("page");
+        } else {
+            console.log("Continuing from page", page);
+        }
     } else {
-      console.log("Continuing from page", page);
+        page = 1;
     }
-  } else {
-    page = 1;
-  }
 
-  let response;
-  let responses = [];
-  let runString;
+    let response;
+    let responses = [];
+    let runString;
 
-  while (page <= 99) {
-    response = await getURL(page);
-    if (!response) {
-      console.log("Error in page " + page + " response, exiting run");
-      return;
-    }
-    responses = response.maps;
-    if (!responses.length) {
-      console.log("No more maps found, exiting run")
-      return;
-    }
-    runString = "";
+    while (page <= 99) {
+        response = await getURL(page);
+        if (!response) {
+            console.log("Error in page " + page + " response, exiting run");
+            return;
+        }
+        responses = response.maps;
+        if (!responses.length) {
+            console.log("No more maps found, exiting run");
+            return;
+        }
+        runString = "";
 
-    for (const one of responses) {
-      var files = fs.readdirSync(directory).filter(fn => fn.startsWith(one.key));
-      if (!files.length) {
-        runString = "start beatsaver://" + one.key;
-        console.log("Downloading", one.key);
-        shellExec.exec(runString);
+        for (const one of responses) {
+            var files = fs.readdirSync(directory).filter((fn) => fn.startsWith(one.key));
+            if (!files.length) {
+                runString = "start beatsaver://" + one.key;
+                console.log("Downloading", one.key);
+                shellExec.exec(runString);
+                await sleep(8000);
+            } else {
+                console.log("Skipping", one.key);
+            }
+        }
+        page = page + 1;
+        await storage.setItem("page", page);
         await sleep(8000);
-      } else {
-        console.log("Skipping", one.key);
-      }
     }
-    page = page + 1;
-    await storage.setItem("page", page);
-    await sleep(8000);
-  }
 }
 
 async function getURL(urlPage) {
-  let body = "{\"page\":" + urlPage + ",\"minStar\":0,\"maxStar\":14,\"sortBy\":\"date\",\"search\":\"\"}";
-  let res = await fetch("https://beat-savior.herokuapp.com/api/maps/ranked/", {
-    "headers": {
-      "content-type": "application/json;charset=UTF-8",
-    },
-    "body": body,
-    "method": "POST"
-  });
-  if (res.status === 200) {
-    console.log("Fetched page", urlPage);
-    return res.json();
-  } else {
-    console.log("Error fetching page " + urlPage + ", exiting run");
-    return;
-  }
+    let body = '{"page":' + urlPage + ',"minStar":0,"maxStar":14,"sortBy":"date","search":""}';
+    let res = await fetch("https://beat-savior.herokuapp.com/api/maps/ranked/", {
+        headers: {
+            "content-type": "application/json;charset=UTF-8",
+        },
+        body: body,
+        method: "POST",
+    });
+    if (res.status === 200) {
+        console.log("Fetched page", urlPage);
+        return res.json();
+    } else {
+        console.log("Error fetching page " + urlPage + ", exiting run");
+        return;
+    }
 }
 
 function restart(query) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  return new Promise(resolve => rl.question(query, ans => {
-    rl.close();
-    resolve(ans);
-  }));
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    return new Promise((resolve) =>
+        rl.question(query, (ans) => {
+            rl.close();
+            resolve(ans);
+        })
+    );
 }
 
 async function sleep(ms) {
-  return new Promise(res => setTimeout(res, ms));
+    return new Promise((res) => setTimeout(res, ms));
 }
